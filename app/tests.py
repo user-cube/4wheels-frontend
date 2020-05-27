d3a17b3f467a6140d8b7a798832e9de7156dc9ca
Rui Coelho <ruicoelho@ua.pt>
2020-05-27 01:26:18 +0100

    pls kill me - II

--- app/tests.py
+++ app/tests.py
@@ -4,10 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class HomepageLoggedOut(TestCase):
     def setUp(self) -> None:
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
         self.vars = {}
 
     def tearDown(self) -> None:
