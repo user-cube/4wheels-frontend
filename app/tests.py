a256c33c9e550b7e68fcd86d3ea2904f0707393c
Rui Coelho <ruicoelho@ua.pt>
2020-05-28 20:03:07 +0100

    add import

--- app/tests.py
+++ app/tests.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 # from django.test import TestCase
